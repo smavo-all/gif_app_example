b77329c3dc9f880e63e9fe77001b07c9af46104d
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-09-08 17:22:53 -0500

    Add Component AddCategory, conexion entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Piece', 'Shigeki no kioyin', 'Bleach'];
+    const [categories, setCategories] = useState(['One Piece', 'Shigeki no kioyin', 'Bleach']);
+
+    // const handleAdd = () => {
+    //     setCategories(['Demo de prueba', ...categories]);
+    //     console.log(categories)
+
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory  setCategories={setCategories} />
             <hr />
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}> {category}</li>
+                    })
+                }
+            </ol>
+
         </>
     )
 }
---
+++ src/components/AddCategory.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({setCategories}) => {
+
+    const [inputValue, setinputValue] = useState('');
+
+    const handlerInputChange = (e) => {
+        console.log(e.target.value);
+        setinputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('Submit Hecho')
+
+        if(inputValue.trim().length > 2){
+            setCategories(categorias => [...categorias, inputValue]);
+            setinputValue('');
+        }
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>{inputValue}</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handlerInputChange}
+            />
+        </form>
+    )
+}
+
+AddCategory.prototype = {
+    setCategories: PropTypes.func.isRequired
+}
