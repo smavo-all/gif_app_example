6b76e3d3ca1444c0f5eff4620cd0561f3b8620ed
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-09-08 20:16:14 -0500

    Add folder docs

---
+++ docs/precache-manifest.be9e81c2c5f48064753bfb135bf41cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272c0ac5db765e97d5ad61a43c2655a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb18ab7db8423129da52",
+    "url": "/static/css/main.f43f79d9.chunk.css"
+  },
+  {
+    "revision": "bb65f802b3b47f9d086e",
+    "url": "/static/js/2.41bfe2ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41bfe2ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb18ab7db8423129da52",
+    "url": "/static/js/main.ca4d9a1a.chunk.js"
+  },
+  {
+    "revision": "0718095deaac241e41d3",
+    "url": "/static/js/runtime-main.64c4dae3.js"
+  }
+]);
